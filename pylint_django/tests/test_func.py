8fde23c649ab225e62ce3c4a548088931b4af5b5
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-11-08 21:07:21 +0100

    Use the latest instalement of functional tests classes

--- pylint_django/tests/test_func.py
+++ pylint_django/tests/test_func.py
@@ -1,46 +1,17 @@
-import csv
 import os
 import pickle
 import sys
 from pathlib import Path
 
-import pylint
 import pytest
+from _pytest.config import Config
+from pylint.testutils import FunctionalTestFile, LintModuleTest
+from pylint.testutils.functional import get_functional_test_files_from_directory
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pylint_django.tests.settings")
 
 HERE = Path(__file__).parent
-
-try:
-    # pylint 2.5: test_functional has been moved to pylint.testutils
-    from pylint.testutils import FunctionalTestFile, LintModuleTest, lint_module_test
-
-    if "test" not in csv.list_dialects():
-
-        class TestDialect(csv.excel):
-            delimiter = ":"
-            lineterminator = "\n"
-
-        csv.register_dialect("test", TestDialect)
-
-    lint_module_test.PYLINTRC = HERE / "testing_pylint.rc"
-except (ImportError, AttributeError):
-    # specify directly the directory containing test_functional.py
-    test_functional_dir = os.getenv("PYLINT_TEST_FUNCTIONAL_DIR", "")
-
-    # otherwise look for it in ~/pylint/tests - pylint 2.4
-    # this is the pylint git checkout dir, not the pylint module dir
-    if not os.path.isdir(test_functional_dir):
-        test_functional_dir = os.path.join(os.getenv("HOME", "/home/travis"), "pylint", "tests")
-
-    # or site-packages/pylint/test/ - pylint before 2.4
-    if not os.path.isdir(test_functional_dir):
-        test_functional_dir = os.path.join(os.path.dirname(pylint.__file__), "test")
-
-    sys.path.append(test_functional_dir)
-
-    from test_functional import FunctionalTestFile, LintModuleTest
-
+FUNCTIONAL_DIR = HERE / "functional"
 
 sys.path += [str(p.absolute()) for p in (HERE, HERE / "../../", HERE / "input")]
 
@@ -50,11 +21,8 @@ class PylintDjangoLintModuleTest(LintModuleTest):
     Only used so that we can load this plugin into the linter!
     """
 
-    def __init__(self, test_file):
-        # if hasattr(test_file, 'option_file') and test_file.option_file is None:
-        # pylint: disable=super-with-arguments
-        # TODO Fix this and the CI (?)
-        super(PylintDjangoLintModuleTest, self).__init__(test_file)  # noqa: UP008
+    def __init__(self, test_file: FunctionalTestFile, config: Config | None = None) -> None:
+        super().__init__(test_file, config)
         self._linter.load_plugin_modules(["pylint_django"])
         self._linter.load_plugin_configuration()
 
@@ -65,59 +33,34 @@ class PylintDjangoMigrationsTest(PylintDjangoLintModuleTest):
     pylint_django.checkers.migrations into the linter!
     """
 
-    def __init__(self, test_file):
-        super().__init__(test_file)
+    def __init__(self, test_file: FunctionalTestFile, config: Config | None = None) -> None:
+        super().__init__(test_file, config)
         self._linter.load_plugin_modules(["pylint_django.checkers.migrations"])
         self._linter.load_plugin_configuration()
 
 
-def get_tests(input_dir="input", sort=False):
-    def _file_name(test):
-        return test.base
-
-    input_dir = HERE / input_dir
-
-    suite = []
-    for fname in input_dir.iterdir():
-        if fname.name != "__init__.py" and fname.name.endswith(".py"):
-            suite.append(FunctionalTestFile(str(input_dir.absolute()), str(fname.absolute())))
-
-    # when testing the migrations plugin we need to sort by input file name
-    # because the plugin reports the errors in close() which appends them to the
-    # report for the last file in the list
-    if sort:
-        suite.sort(key=_file_name)
-
-    return suite
+TESTS = get_functional_test_files_from_directory(FUNCTIONAL_DIR)
 
 
-TESTS = get_tests()
-TESTS.extend(get_tests("input/models"))
-TESTS_NAMES = [t.base for t in TESTS]
-
-
-@pytest.mark.parametrize("test_file", TESTS, ids=TESTS_NAMES)
-def test_everything(test_file):
-    # copied from pylint.tests.test_functional.test_functional
+@pytest.mark.parametrize("test_file", TESTS, ids=lambda x: x.base)
+def test_everything(test_file: FunctionalTestFile):
     lint_test = PylintDjangoLintModuleTest(test_file)
     lint_test.setUp()
     lint_test.runTest()
 
 
 # NOTE: define tests for the migrations checker!
-MIGRATIONS_TESTS = get_tests("input/migrations", True)
-MIGRATIONS_TESTS_NAMES = [t.base for t in MIGRATIONS_TESTS]
+MIGRATIONS_TESTS = get_functional_test_files_from_directory(FUNCTIONAL_DIR / "migrations")
 
 
-@pytest.mark.parametrize("test_file", MIGRATIONS_TESTS, ids=MIGRATIONS_TESTS_NAMES)
-def test_migrations_plugin(test_file):
-    lint_test = PylintDjangoMigrationsTest(test_file)
+@pytest.mark.parametrize("migration_test_file", MIGRATIONS_TESTS, ids=lambda x: x.base)
+def test_migrations_plugin(migration_test_file: FunctionalTestFile) -> None:
+    lint_test = PylintDjangoMigrationsTest(migration_test_file)
     lint_test.setUp()
     lint_test.runTest()
 
 
-@pytest.mark.parametrize("test_file", MIGRATIONS_TESTS[:1], ids=MIGRATIONS_TESTS_NAMES[:1])
-@pytest.mark.skip  # TODO currently skipped because ArgParser which pylint uses is not pickable.
+@pytest.mark.parametrize("test_file", MIGRATIONS_TESTS[:1], ids=lambda x: x.base)
 def test_linter_should_be_pickleable_with_pylint_django_plugin_installed(test_file):
     lint_test = PylintDjangoMigrationsTest(test_file)
     lint_test.setUp()
